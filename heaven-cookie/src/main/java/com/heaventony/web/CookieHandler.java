6cbbb084eef110e8ed9633fbb5f0da2df7da2db5
tonywell <tongwei1985@gmail.com>
2017-04-05 11:07:46 +0800

    test license

---
+++ heaven-cookie/src/main/java/com/heaventony/web/CookieHandler.java
@@ -0,0 +1,36 @@
+package com.heaventony.web;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: tonywill
+ * Email: tongwei1985@gmail.com
+ * Date: 2017/4/1
+ * Time: 下午2:31
+ */
+public class CookieHandler {
+
+    private static final int DEFAULT_MAXAGE = 0;
+    public CookieHandler() {
+
+    }
+
+    public CookieHandler(String name, String value, HttpServletResponse response) {
+        generateCookie(name, value, response, DEFAULT_MAXAGE);
+    }
+
+    public CookieHandler (String name, String value,HttpServletResponse response, int maxAge) {
+         generateCookie(name, value, response, maxAge);
+    }
+
+    public void generateCookie(String name, String value, HttpServletResponse response, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setMaxAge(maxAge);
+        response.addCookie(cookie);
+    }
+
+
+
+}
